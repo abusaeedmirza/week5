2b6a296f75fd3f1df9a26d774b479db6cb797183
mirza <36728610+abusaeedmirza@users.noreply.github.com>
2023-07-06 15:17:15 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import L from "leaflet";
 import "./styles.css";
 import "./leaflet.css";
 import L from "./leaflet";
+import L from "leaflet";
 const StartMap = (data) => {
   let map = L.map("map", {
     minZoom: -3,
